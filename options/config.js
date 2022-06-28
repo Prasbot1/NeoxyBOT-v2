b25842d1c81160f66700ece7d3a8b6daebaf3171
Prasbot1 <107738150+Prasbot1@users.noreply.github.com>
2022-06-28 08:46:58 +0700

    Update config.js

--- options/config.js
+++ options/config.js
@@ -11,31 +11,31 @@ global.thum = fs.readFileSync("./plugins/media/image/thumb.jpg")
 global.log0 = fs.readFileSync("./plugins/media/image/thumb.jpg")
 global.err4r = fs.readFileSync("./plugins/media/image/thumb.jpg")
 global.locimg = fs.readFileSync("./plugins/media/image/loc.jpg")
-global.owner = ['6283834558105', '6285789004732']
-global.numberOwner = "6283834558105"
-global.group = "https://chat.whatsapp.com/E3zewfxrc5pKE6Rzb3BuqG"
-global.rkyt = []
-global.banUser = ['6281366679021@s.whatsapp.net']
+global.owner = ['6289666747744', '6289666747744']
+global.numberOwner = "6289666747744"
+global.group = "https://chat.whatsapp.com/Lr6KNr05PEHAy7romaot9N"
+global.rkyt = ["6289666747744"]
+global.banUser = ['6289666747744@s.whatsapp.net']
 global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
 global.wlcm = ['120363043965529911@g.us','120363023112413670@g.us','19054091487-1424650263@g.us','120363022988111568@g.us']
 global.gcrevoke = ['19054091487-1424650263@g.us']
-global.packname = "Neoxy Bot"
-global.author = "Lexxy Official"
+global.packname = "XyuziMD"
+global.author = "PraszWIBU"
 global.sessionName = "session"
-global.footer = "© Created By Lexxy Official"
-global.linkgrupss = "https://chat.whatsapp.com/E3zewfxrc5pKE6Rzb3BuqG"
+global.footer = "© PraszWibu"
+global.linkgrupss = "https://chat.whatsapp.com/Lr6KNr05PEHAy7romaot9N"
 global.antitags = true
 global.txtSewabot = "1-Minggu : Rp5.000\n2-Minggu : Rp.10.000\n1-Bulan : Rp.20.000\n2-Bulan : Rp.30.000\n3-Bulan : Rp40.000"
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.mess = {
-    success: 'Success',
+    success: 'Done',
     admin: 'Fitur Khusus Admin Group!',
     botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
     owner: 'Fitur Khusus Owner Bot',
     group: 'Fitur Digunakan Hanya Untuk Group!',
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Loading...',
+    wait: 'Wait a Minute Second👏',
     linkm: 'Linknya Mana Kak?',
     ban: 'Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner'
 }
